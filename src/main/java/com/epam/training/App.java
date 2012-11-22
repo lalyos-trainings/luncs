4630e1d091cd960bbc9bd5082229743feacab2fb
epam-af <fekete.attila@mailbox.hu>
2012-11-22 13:32:54 +0100

    resolving first conflict

--- src/main/java/com/epam/training/App.java
+++ src/main/java/com/epam/training/App.java
@@ -1,11 +1,14 @@
 package com.epam.training;
 
+
 import java.util.Collection;
 
 import com.epam.training.domain.Food;
+
 import com.epam.training.domain.Restaurant;
 import com.epam.training.service.InMemoryRestaurantRepository;
 import com.epam.training.service.RestaurantRepository;
+
 
 public class App {
 
