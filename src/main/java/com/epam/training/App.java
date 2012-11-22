09c6e7ec3e225033bb21d2bb7f3e531253a22eed
lajos papp <lalyos@yahoo.com>
2012-11-22 13:12:20 +0100

    main app wird hardwired dependecies created

---
+++ src/main/java/com/epam/training/App.java
@@ -0,0 +1,28 @@
+package com.epam.training;
+
+import java.util.Collection;
+
+import com.epam.training.domain.Food;
+import com.epam.training.domain.Restaurant;
+import com.epam.training.service.InMemoryRestaurantRepository;
+import com.epam.training.service.RestaurantRepository;
+
+public class App {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        RestaurantRepository repo = new InMemoryRestaurantRepository();
+        for (Restaurant restaurant : repo.getAllRestaurants()) {
+            System.out.println("=== next resti: " + restaurant.getName());
+            Collection<Food> foods = restaurant.getMenu().getFoods();
+            for (Food food : foods) {
+                System.out.println("  " + food.getName());
+            }
+            
+        }
+
+    }
+
+}
