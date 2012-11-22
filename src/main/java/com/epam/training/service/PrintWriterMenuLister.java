4b8b4260195aae5b58b1a998ddfc49035007b58f
lajos papp <lalyos@yahoo.com>
2012-11-22 22:17:47 +0100

    sysoutMenuLister generalized into PrintWriterMenuLister

---
+++ src/main/java/com/epam/training/service/PrintWriterMenuLister.java
@@ -0,0 +1,38 @@
+package com.epam.training.service;
+
+import java.io.PrintWriter;
+import java.util.Collection;
+
+import com.epam.training.domain.Food;
+import com.epam.training.domain.Restaurant;
+
+public class PrintWriterMenuLister implements MenuLister {
+
+    private PrintWriter writer;
+    private RestaurantRepository repo;
+    
+    public PrintWriterMenuLister(PrintWriter writer) {
+        this.writer = writer;
+    }
+    
+    public void doList() {
+        for (Restaurant restaurant : repo.getAllRestaurants()) {
+            writer.println("=== next resti: " + restaurant.getName());
+            Collection<Food> foods = restaurant.getMenu().getFoods();
+            for (Food food : foods) {
+                writer.println("  " + food.getName());
+            }
+        }
+
+    }
+
+    public RestaurantRepository getRepo() {
+        return repo;
+    }
+
+    public void setRepo(RestaurantRepository repo) {
+        this.repo = repo;
+    }
+
+
+}
--- src/main/java/com/epam/training/service/SysoutMenuLister.java
+++ src/main/java/com/epam/training/service/SysoutMenuLister.java
@@ -1,32 +1,11 @@
 package com.epam.training.service;
 
-import java.util.Collection;
+import java.io.PrintWriter;
 
-import com.epam.training.domain.Food;
-import com.epam.training.domain.Restaurant;
 
-public class SysoutMenuLister implements MenuLister {
+public class SysoutMenuLister extends PrintWriterMenuLister {
 
-    private RestaurantRepository repo;
-    
-    /* (non-Javadoc)
-     * @see com.epam.training.service.MenuLister#doList()
-     */
-    public void doList() {
-        for (Restaurant restaurant : repo.getAllRestaurants()) {
-            System.out.println("=== next resti: " + restaurant.getName());
-            Collection<Food> foods = restaurant.getMenu().getFoods();
-            for (Food food : foods) {
-                System.out.println("  " + food.getName());
-            }
-        }
-    }
-
-    public RestaurantRepository getRepo() {
-        return repo;
-    }
-
-    public void setRepo(RestaurantRepository repo) {
-        this.repo = repo;
+    public SysoutMenuLister() {
+        super(new PrintWriter(System.out));
     }
 }
