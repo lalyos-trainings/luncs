eaab50b224e58de2ebbd328ba716abe401005319
lajos papp <lalyos@yahoo.com>
2012-11-23 13:29:07 +0100

    switching from BeanFactory to ApplicationContext

--- src/main/java/com/epam/training/App.java
+++ src/main/java/com/epam/training/App.java
@@ -1,6 +1,8 @@
 package com.epam.training;
 
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 
@@ -12,8 +14,9 @@ public class App {
      * @param args
      */
     public static void main(String[] args) {
-        XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        MenuLister lister = factory.getBean(MenuLister.class);
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+        
+        MenuLister lister = ctx.getBean(MenuLister.class);
         
         lister.doList();
         
