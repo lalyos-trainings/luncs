1b8aaf1c83e57e030fbfd1c3dc90ca89faf46a4d
Kovacs Tunde <kovacs_bethsabe89@yahoo.com>
2012-11-23 11:15:08 +0100

    added spring to project1

---
+++ src/main/java/com/epam/training/service/PrintWriterMenuLister.java
@@ -0,0 +1,42 @@
+package com.epam.training.service;
+
+import java.io.PrintWriter;
+import java.util.Collection;
+
+import com.epam.training.domain.Food;
+import com.epam.training.domain.Restaurant;
+
+public class PrintWriterMenuLister implements MenuLister {
+    
+    private RestaurantRepository repo;
+    private PrintWriter printWriter;
+    
+    public PrintWriterMenuLister(PrintWriter printWriter){        
+        this.printWriter = printWriter;
+    }
+
+    public void setRestaurantRepository(RestaurantRepository repo) {
+        this.repo = repo;        
+    }
+
+    public void doList() {
+        for (Restaurant restaurant : repo.getAllRestaurants()) {
+            printWriter.println("=== next resti: " + restaurant.getName());
+            Collection<Food> foods = restaurant.getMenu().getFoods();
+            for (Food food : foods) {
+                printWriter.println("  " + food.getName());
+            }
+            
+        }
+        
+    }
+
+    public RestaurantRepository getRepo() {
+        return repo;
+    }
+
+    public void setRepo(RestaurantRepository repo) {
+        this.repo = repo;
+    }
+
+}
