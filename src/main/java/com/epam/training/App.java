dd168a3e9590266d3907cb9ba39eca338efe229e
Kovacs Tunde <kovacs_bethsabe89@yahoo.com>
2012-11-23 14:45:10 +0100

    updated pom.xml to use slf4j, and some other stuff

--- src/main/java/com/epam/training/App.java
+++ src/main/java/com/epam/training/App.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.epam.training.service.MenuLister;
+import com.epam.training.service.RestaurantRepository;
 
 
 public class App {
@@ -13,11 +14,13 @@ public class App {
      */
     public static void main(String[] args) {      
         ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml","ching.xml","kfc.xml");
-                
+        
+      //  ApplicationContext ctx = new ClassPathXmlApplicationContext("ching.xml");
         MenuLister lister = ctx.getBean(MenuLister.class);
         
         lister.doList();
         
+   
     }
 
 }
