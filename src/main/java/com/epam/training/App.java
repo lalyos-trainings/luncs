ae8446c414fa5cac92830284e71214a3e0936165
bandi szabo <andras.szabo@outlook.com>
2012-11-23 14:48:27 +0100

    logging set

--- src/main/java/com/epam/training/App.java
+++ src/main/java/com/epam/training/App.java
@@ -1,8 +1,8 @@
 package com.epam.training;
 
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.epam.training.service.MenuLister;
 
@@ -12,8 +12,8 @@ public class App {
      * @param args
      */
     public static void main(String[] args) {
-        XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        MenuLister lister = factory.getBean(MenuLister.class);
+       ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml", "csing-csang.xml", "kfc.xml");
+       MenuLister lister = ctx.getBean(MenuLister.class);
         
         lister.doList();
         
