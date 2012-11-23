793f68ea1053791954ba59dcda5ec6a5ea5af75c
Kovacs Tunde <kovacs_bethsabe89@yahoo.com>
2012-11-23 13:27:07 +0100

    added kfc.xml

--- src/main/java/com/epam/training/App.java
+++ src/main/java/com/epam/training/App.java
@@ -1,7 +1,7 @@
 package com.epam.training;
 
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.epam.training.service.MenuLister;
 
@@ -11,12 +11,10 @@ public class App {
     /**
      * @param args
      */
-    public static void main(String[] args) {
-      
-        @SuppressWarnings("deprecation")
-        XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        
-        MenuLister lister = factory.getBean(MenuLister.class);
+    public static void main(String[] args) {      
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml","ching.xml","kfc.xml");
+                
+        MenuLister lister = ctx.getBean(MenuLister.class);
         
         lister.doList();
         
