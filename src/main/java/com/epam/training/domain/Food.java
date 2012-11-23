176b1aa9653e1e390996e916202ee8aacf5b6478
lajos papp <lalyos@yahoo.com>
2012-11-23 11:37:19 +0100

    default constructor is added

--- src/main/java/com/epam/training/domain/Food.java
+++ src/main/java/com/epam/training/domain/Food.java
@@ -5,6 +5,8 @@ public class Food {
     private String description;
     private int price;
 
+    public Food() {}
+    
     public Food(String name, int price) {
         this.name = name;
         this.price = price;
