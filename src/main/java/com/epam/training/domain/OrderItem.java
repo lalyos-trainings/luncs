ccb622ff8c9ba9fe467243e91d949f5310d65092
lajos papp <lalyos@yahoo.com>
2012-11-23 22:30:36 +0100

    Adding Order-OrderItem

---
+++ src/main/java/com/epam/training/domain/OrderItem.java
@@ -0,0 +1,27 @@
+package com.epam.training.domain;
+
+public class OrderItem {
+    private int quantity;
+    private Food food;
+    
+    public OrderItem(int quantity, Food food) {
+        super();
+        this.quantity = quantity;
+        this.food = food;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public Food getFood() {
+        return food;
+    }
+    public void setFood(Food food) {
+        this.food = food;
+    }
+    public void addQuantity(int additional) {
+        this.quantity += additional;
+    }
+}
