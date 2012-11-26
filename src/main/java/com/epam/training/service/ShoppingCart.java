55d7c46f7e20dbfdb9a31f87b9a99d68ae74d621
bandi szabo <andras.szabo@outlook.com>
2012-11-26 10:37:08 +0100

    refactored final

--- src/main/java/com/epam/training/service/ShoppingCart.java
+++
@@ -1,9 +0,0 @@
-package com.epam.training.service;
-
-public interface ShoppingCart {
-    void addFood(int foodId, int quantity);
-    void setCustomer(String customer);
-    void setDeliveryAddress(String city, String street, String zip, String country);
-    void setBillingaddress(String city, String street, String zip, String country);
-    void checkOut();
-}
