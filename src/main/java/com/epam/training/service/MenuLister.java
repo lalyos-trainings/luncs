03703d8613e004b20e3b8e3bee5e5776d5baa45a
Kovacs Tunde <kovacs_bethsabe89@yahoo.com>
2012-11-26 10:32:16 +0100

    refactored package to com.acme

--- src/main/java/com/epam/training/service/MenuLister.java
+++
@@ -1,9 +0,0 @@
-package com.epam.training.service;
-
-public interface MenuLister {
-
-    public abstract void setRestaurantRepository(RestaurantRepository repo);
-
-    public abstract void doList();
-
-}
