bbcb798c11e916b4dd2604d3fd38f5bd982c0b86
lajos papp <lalyos@yahoo.com>
2012-11-26 15:18:15 +0100

    introducing language property

--- src/main/java/com/acme/training/service/PrintWriterMenuLister.java
+++ src/main/java/com/acme/training/service/PrintWriterMenuLister.java
@@ -4,6 +4,8 @@ import java.io.PrintWriter;
 import java.util.Collection;
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -16,19 +18,31 @@ public class PrintWriterMenuLister implements MenuLister, ApplicationContextAwar
     private PrintWriter writer;
     private RestaurantRepository repo;
     private ApplicationContext ctx;
+    private Locale locale;
+    private Logger logger = LoggerFactory.getLogger(PrintWriterMenuLister.class);
     
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public void setLocale(Locale locale) {
+        this.locale = locale;
+    }
+
     public PrintWriterMenuLister(PrintWriter writer) {
         this.writer = writer;
     }
     
     public void doList() {
-        String nextRestMsg = ctx.getMessage("rest.next", null, new Locale("hu"));
+        logger.info("locale: " + locale);
+        String nextRestMsg = ctx.getMessage("rest.next", null, locale);
+        String foodName = ctx.getMessage("food.name", null, locale);
         for (Restaurant restaurant : repo.getAllRestaurants()) {
             
             writer.println("=== " + nextRestMsg + ": " + restaurant + "\n===");
             Collection<Food> foods = restaurant.getMenu().getFoods();
             for (Food food : foods) {
-                writer.println(food);
+                writer.println(String.format("%s: %-15s [%5d]",foodName , food.getName(), food.getPrice()));
             }
         }
 
