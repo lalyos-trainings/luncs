ffbd0ca198be62d18ebfa76354acc5a7aa9c4af2
Farkas David <sagrath.grash@gmail.com>
2012-11-27 11:33:23 +0100

    repo in propertie file

--- src/main/java/com/acme/training/service/InMemoryRestaurantRepository.java
+++ src/main/java/com/acme/training/service/InMemoryRestaurantRepository.java
@@ -1,7 +1,6 @@
 package com.acme.training.service;
 
 import org.springframework.beans.factory.BeanNameAware;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import com.acme.training.domain.Food;
@@ -9,7 +8,6 @@ import com.acme.training.domain.Menu;
 import com.acme.training.domain.Restaurant;
 
 @Component("memoryRepo")
-@Qualifier("memory")
 public class InMemoryRestaurantRepository extends AbstractRestaurantRepository implements BeanNameAware {
 
     public InMemoryRestaurantRepository() {
--- src/main/java/com/acme/training/service/SysoutMenuLister.java
+++ src/main/java/com/acme/training/service/SysoutMenuLister.java
@@ -2,7 +2,9 @@ package com.acme.training.service;
 
 import java.io.PrintWriter;
 
+import org.springframework.stereotype.Component;
 
+@Component
 public class SysoutMenuLister extends PrintWriterMenuLister {
 
     public SysoutMenuLister() {
