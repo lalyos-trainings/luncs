c0f59bb3a60d6ec14aa4f6de1dda9415d3d3fc1b
Kovacs Tunde <kovacs_bethsabe89@yahoo.com>
2012-11-27 09:57:49 +0100

    added abstract repo class_1

---
+++ src/main/java/com/acme/service/AbstractRestaurantRepository.java
@@ -0,0 +1,42 @@
+package com.acme.service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.BeanNameAware;
+
+import com.acme.domain.Food;
+import com.acme.domain.Restaurant;
+
+public class AbstractRestaurantRepository {
+
+    protected Map<String, Restaurant> restaurantMap = new HashMap<String, Restaurant>();
+    protected ArrayList<Food> foodList = new ArrayList<Food>();
+    private Logger logger = LoggerFactory.getLogger(InMemoryRestaurantRepository.class);
+
+    public AbstractRestaurantRepository() {
+        super();
+    }
+
+    public Collection<Restaurant> getAllRestaurants() {
+        return restaurantMap.values();
+    }
+
+    public Food getFoodbyId(int id) {        
+        for (Food f: foodList){
+            if (f.getId() == id){
+                return f;
+            }
+        }
+        return null;
+    }
+
+    public void setBeanName(String arg0) {
+        logger.info(arg0);
+    }
+
+}
