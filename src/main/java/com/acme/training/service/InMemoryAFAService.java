049bbe026372cedb859552dfd66efbb77d816a1d
bandi szabo <andras.szabo@outlook.com>
2012-11-27 14:47:59 +0100

    AFA beallitasa propertibol

--- src/main/java/com/acme/training/service/InMemoryAFAService.java
+++ src/main/java/com/acme/training/service/InMemoryAFAService.java
@@ -1,5 +1,6 @@
 package com.acme.training.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
@@ -7,7 +8,9 @@ import com.acme.training.domain.Order;
 
 @Component
 public class InMemoryAFAService implements ApplicationListener<OrderEvent>{
-
+    
+    @Value("${VAT}")
+    double VAT;
     int total = 0;
     public void onApplicationEvent(OrderEvent event) {        
         Order order = event.getOrder();
@@ -16,7 +19,7 @@ public class InMemoryAFAService implements ApplicationListener<OrderEvent>{
     public void doNAVStatistics(){
         System.out.println("===========AFA TOTAL===========");
         System.out.println("Osszes bevet : " + total);  
-        System.out.println("AFA bevet : " + total * 0.2);
+        System.out.println("AFA bevet : " + total * VAT / 100);
     }
 
 }
