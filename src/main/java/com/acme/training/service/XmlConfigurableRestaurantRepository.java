86dd35065fd17b84b9eca01b29beb03bbf8c034a
nethy <bla@bla.com>
2012-11-27 10:42:14 +0100

    delete unnecceseary override

--- src/main/java/com/acme/training/service/XmlConfigurableRestaurantRepository.java
+++ src/main/java/com/acme/training/service/XmlConfigurableRestaurantRepository.java
@@ -1,17 +1,10 @@
 package com.acme.training.service;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-
-import com.acme.training.domain.Restaurant;
 
 @Component("XmlRepo")
 @Qualifier("xml")
 public class XmlConfigurableRestaurantRepository extends AbstractRestaurantRepository {
-    @Override
-    public void setRestaurantMap(Map<String, Restaurant> restaurantMap) {
-        this.restaurantMap = restaurantMap;
-    }
+
 }
