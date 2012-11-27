dccef4fdb480f5e2d10bdb96bca004f3b25bbfdd
lostface <lostface@fastmail.fm>
2012-11-27 11:23:57 +0100

    beans xml alias intro

--- src/main/java/com/acme/training/service/InMemoryShoppingCart.java
+++ src/main/java/com/acme/training/service/InMemoryShoppingCart.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -25,7 +24,6 @@ public class InMemoryShoppingCart implements ShoppingCart, BeanNameAware
     private OrderService orderService;
     
     @Autowired
-    @Qualifier("memory")
     private RestaurantRepository repo;
 
     public InMemoryShoppingCart() 
