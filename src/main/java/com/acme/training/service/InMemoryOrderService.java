7d7751b3d2effcf9a5a1706aa88b7a9b9d6031d0
lajos papp <lalyos@yahoo.com>
2012-11-27 13:21:03 +0100

    implementing OrderStatistic by introducing ApplicationEvent mechanism

--- src/main/java/com/acme/training/service/InMemoryOrderService.java
+++ src/main/java/com/acme/training/service/InMemoryOrderService.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import com.acme.training.domain.Order;
@@ -12,9 +14,13 @@ import com.acme.training.domain.Order;
 public class InMemoryOrderService implements OrderService {
 
     Map<String, Order> orders = new HashMap<String, Order>();
+    @Autowired
+    private ApplicationContext context;
     
     public void doOrder(Order order) {
         orders.put(order.getId(), order);
+        OrderEvent event = new OrderEvent(this, order);
+        context.publishEvent(event);
     }
     
     public Collection<Order> getAllOrder() {
---
+++ src/main/java/com/acme/training/service/InMemoryStatisticService.java
@@ -0,0 +1,39 @@
+package com.acme.training.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import com.acme.training.domain.OrderItem;
+
+@Component
+public class InMemoryStatisticService implements ApplicationListener<OrderEvent>{
+
+    Map<Integer, OrderItem> foodStatistic = new HashMap<Integer, OrderItem>();
+    public void onApplicationEvent(OrderEvent event) {
+        List<OrderItem> items = event.getOrder().getItems();
+        for (OrderItem item : items) {
+            doStatistic(item);
+        }
+    }
+    private void doStatistic(OrderItem item) {
+        Integer foodId = item.getFood().getId();
+        
+        OrderItem orderItem = foodStatistic.get(foodId);
+        if (orderItem != null) {
+            orderItem.addQuantity(item.getQuantity());
+        } else {
+            foodStatistic.put(foodId, item);
+        }
+    }
+
+    public void printStatistic() {
+        System.out.println("==== STATISTIC:");
+        for (OrderItem item : foodStatistic.values()) {
+            System.out.println(String.format(" %20s : %-4d" , item.getFood().getName(), item.getQuantity()));
+        }
+    }
+}
