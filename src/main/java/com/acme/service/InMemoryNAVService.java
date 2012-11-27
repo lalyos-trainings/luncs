61c55c5740ac1026de873b7eaf0d6bf733e5e5c0
Kovacs Tunde <kovacs_bethsabe89@yahoo.com>
2012-11-27 14:50:27 +0100

    put vat value into property file

--- src/main/java/com/acme/service/InMemoryNAVService.java
+++ src/main/java/com/acme/service/InMemoryNAVService.java
@@ -1,37 +1,20 @@
 package com.acme.service;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
-
-import com.acme.domain.OrderItem;
-import com.acme.domain.Restaurant;
 
 @Component
 public class InMemoryNAVService implements  ApplicationListener<OrderEvent> {
 
-    private Map<String, Integer> incomeMap = new HashMap<String, Integer>();
     private int uberGrandTotal;
-    
+    @Value("${vat}")
+    private String vat;
     
     public void onApplicationEvent(OrderEvent event) {
         int total = event.getOrder().getGrandTotal();
         uberGrandTotal +=total;        
-    }
-
-    private void addToPriceList(OrderItem oi) {
-        Restaurant rest = oi.getFood().getRestaurant();
-        Integer price = incomeMap.get(rest);
-        if(price!=null){
-            incomeMap.put(rest.getName(), price+oi.getFood().getPrice());
-            
-        }else{
-            incomeMap.put(rest.getName(), oi.getFood().getPrice());
-        }
     }
     
     public int getUberGrandTotal(){
@@ -39,18 +22,9 @@ public class InMemoryNAVService implements  ApplicationListener<OrderEvent> {
     }
     
     public double getTotalVAT(){
-        return uberGrandTotal * 0.25;
+        return uberGrandTotal * Double.parseDouble(vat);
     }
-    
-    public void printRestaurantsIncome(){
-//        System.out.println("===RESTAURANT INCOME===");
-//        Iterator it = incomeMap.entrySet().iterator();
-//        while (it.hasNext()) {
-//            Map.Entry pairs = (Map.Entry)it.next();
-//            System.out.println(pairs.getKey() + " = " + pairs.getValue());
-//        }      
-        
-    }
+   
     
     public void printTotalVAT(){
         System.out.println(String.format("Total VAT:%f", getTotalVAT()));
