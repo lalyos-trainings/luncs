d535bb0c9382b25b13ec06ef65b431bde9160a53
lajos papp <lalyos@yahoo.com>
2012-11-27 18:59:29 +0100

    extracting hardcoded VAT into propertyFile

--- src/main/java/com/acme/training/service/NavService.java
+++ src/main/java/com/acme/training/service/NavService.java
@@ -1,11 +1,14 @@
 package com.acme.training.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NavService implements ApplicationListener<OrderEvent> {
 
+    @Value("${vat}")
+    private double vat;
     private int uberGrandTotal = 0;
     
     public void onApplicationEvent(OrderEvent event) {
@@ -18,7 +21,7 @@ public class NavService implements ApplicationListener<OrderEvent> {
     }
     
     public double getTotalVAT() {
-        return uberGrandTotal * 0.25;
+        return uberGrandTotal * vat;
     }
 
 }
