c91d82fbbd933be6a95c732cfe7b3caa0ae7f7b4
lajos papp <lalyos@yahoo.com>
2012-11-27 13:49:35 +0100

    logging added

--- src/main/java/com/acme/training/service/InMemoryOrderService.java
+++ src/main/java/com/acme/training/service/InMemoryOrderService.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
@@ -13,12 +15,15 @@ import com.acme.training.domain.Order;
 @Component
 public class InMemoryOrderService implements OrderService {
 
+    Logger logger = LoggerFactory.getLogger(InMemoryOrderService.class);
     Map<String, Order> orders = new HashMap<String, Order>();
     @Autowired
     private ApplicationContext context;
     
     public void doOrder(Order order) {
+        logger.info("new Order for:" + order.getCustomer());
         orders.put(order.getId(), order);
+        logger.info("# of order:" + orders.keySet().size());
         OrderEvent event = new OrderEvent(this, order);
         context.publishEvent(event);
     }
