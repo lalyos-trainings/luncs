5abc3051422606d75efc0a9b59f96f2a4ec85b85
Peter Dajka <dippede@gmail.com>
2012-11-28 01:15:25 +0100

    add CustomerOrder class

--- src/main/java/com/acme/training/domain/CustomerOrder.java
+++ src/main/java/com/acme/training/domain/CustomerOrder.java
@@ -1,15 +1,37 @@
 package com.acme.training.domain;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.ArrayList;
 
 public class CustomerOrder {
 	
 	private Customer customer;
-	private List<RestaurantOrder> restaurantOrders = new ArrayList<RestaurantOrder>();
+	private Collection<RestaurantOrder> restaurantOrders = new ArrayList<RestaurantOrder>();
 
+	// ToDo: implement printing !!
+	public void printBill(){
+		System.out.println("NOT IMPLEMENTED YET");
+	}
 	
 	
+	public int getTotal(){
+		int total = 0;
+		for ( RestaurantOrder ro: restaurantOrders ){
+			int roTotal = ro.getTotal();
+			total += roTotal;
+		}
+		return total;
+	}
+	
+	public void addRestaurantOrder( RestaurantOrder ro ){
+		restaurantOrders.add(ro);
+	}
+	
+	public String getDeliveryAddress(){
+		String address = customer.getDeliveryAddress();
+		return address;
+	}
 	
 	public Customer getCustomer() {
 		return customer;
@@ -17,10 +39,10 @@ public class CustomerOrder {
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
 	}
-	public List<RestaurantOrder> getRestaurantOrders() {
+	public Collection<RestaurantOrder> getRestaurantOrders() {
 		return restaurantOrders;
 	}
-	public void setRestaurantOrders(List<RestaurantOrder> restaurantOrders) {
+	public void setRestaurantOrders(Collection<RestaurantOrder> restaurantOrders) {
 		this.restaurantOrders = restaurantOrders;
 	}
 	
