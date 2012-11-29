d41e6efee5c5ec17ed7563b037c06c7d7562210e
Peter Dajka <dippede@gmail.com>
2012-11-29 14:37:35 +0100

    add event handling

--- src/main/java/com/acme/training/service/CustomerOrderService.java
+++ src/main/java/com/acme/training/service/CustomerOrderService.java
@@ -5,9 +5,12 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import com.acme.training.domain.CustomerOrder;
+import com.acme.training.domain.RestaurantOrder;
 
 @Component
 public class CustomerOrderService {
@@ -23,6 +26,10 @@ public class CustomerOrderService {
     // name, CustomerOrder
     Map< String, CustomerOrder > shoppingCartMapByName = new HashMap< String, CustomerOrder>();
 
+    @Autowired
+    private ApplicationContext ctx;
+    
+    
     public CustomerOrderService() {
         
     }
@@ -63,53 +70,24 @@ public class CustomerOrderService {
         logger.info("checkout");
         CustomerOrder sc = getShoppingCartById( scId );
         addCustomerOrder(sc);
+        callEvents( sc );
     }
-    
-    // --------- Customer Order 
 
-    
-    
-    // 
     public int addCustomerOrder( CustomerOrder co ){
         shoppingCartMapByName.put( co.getCustomer().getName() , co );
         return co.getId();
     }
-//    
-//    // start a new order and return with id
-//    public int addCustomerOrder( String customerName, String street, String city, String zip, String country){
-//        CustomerOrder customerOrder = new CustomerOrder(customerName, street, city, zip, country);
-//        customerOrders.put( customerOrder.getId(), customerOrder );
-//        shoppingCartMapByName.put( customerName, customerOrder );
-//        return customerOrder.getId();
-//    }
-//    
-//    public int addCustomerOrder( String customerName ){
-//        CustomerOrder customerOrder = new CustomerOrder(customerName);
-//        customerOrders.put( customerOrder.getId(), customerOrder );
-//        shoppingCartMapByName.put( customerName, customerOrder );
-//        return customerOrder.getId();
-//    }
-//    
-//    public CustomerOrder getCustomerOrderById( Integer id ){
-//        CustomerOrder co = customerOrders.get( id );
-//        return co;
-//    }
+
     
-//    public Map<Integer, CustomerOrder> getCustomerOrders() {
-//        return customerOrders;
-//    }
-//
-//    public void setCustomerOrders(Map<Integer, CustomerOrder> customerOrders) {
-//        this.customerOrders = customerOrders;
-//    }
-
-
-//    // for logging
-//    public String getAllCustomerName(){
-//        String names = "";
-//        for ( CustomerOrder co : customerOrders.values() ){
-//            names += ", " + co.getCustomer().getName();
-//        }
-//        return names;
-//    }
+    private void callEvents( CustomerOrder co ){
+        CustomerOrderEvent coEvent = new CustomerOrderEvent(this, co);
+        ctx.publishEvent(coEvent);
+        
+        for( RestaurantOrder ro : co.getRestaurantOrders().values() ){
+            RestaurantOrderEvent roEvent = new RestaurantOrderEvent(this, ro);
+            ctx.publishEvent(roEvent);
+        }
+        
+    }
+    
 }
