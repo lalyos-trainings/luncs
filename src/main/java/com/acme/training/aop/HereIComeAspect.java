79566ee860ec41432489e27af720bdf2c13b7d2d
lajos papp <lalyos@yahoo.com>
2012-12-19 11:23:49 +0100

    MY First Aspect

--- src/main/java/com/acme/training/aop/HereIComeAspect.java
+++ src/main/java/com/acme/training/aop/HereIComeAspect.java
@@ -1,7 +1,15 @@
 package com.acme.training.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
-
+@Aspect
+@Component
 public class HereIComeAspect {
 
+	@Before("execution(* com.acme.training.service.*.*(..) )")
+	public void here() {
+		System.out.println("Here I come ...");
+	}
 }
