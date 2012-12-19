9d8dfd7db2b0442c20a2384ac0e8d8e8cf7f5909
lajos papp <lalyos@yahoo.com>
2012-12-19 13:27:08 +0100

    complex pointcut expressions with @Pointcut definitions

--- src/main/java/com/acme/training/aop/TimerAspect.java
+++ src/main/java/com/acme/training/aop/TimerAspect.java
@@ -3,13 +3,14 @@ package com.acme.training.aop;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class TimerAspect {
 
-	@Around("execution(* com.acme..*.*(..))")
+	@Around("getMethods() && returnsCollectionMethods()")
 	public Object timer(ProceedingJoinPoint joinPoint) {
 		Object ret = null;
 		long startTime = System.currentTimeMillis();
@@ -19,7 +20,22 @@ public class TimerAspect {
 			e.printStackTrace();
 		}
 		long endTime = System.currentTimeMillis();
-		System.out.format("%s method took %d ms%n", joinPoint.getSignature(), (endTime-startTime));
+		System.out.format("%s method took %d ms%n", joinPoint.getSignature(),
+				(endTime - startTime));
 		return ret;
 	}
+
+	@Pointcut("execution(* com.acme..*.*(..))")
+	public void allMethods() {
+
+	}
+
+	@Pointcut("execution(* com.acme..*.get*(..))")
+	public void getMethods() {
+	}
+
+	@Pointcut("execution(java.util.Collection com.acme..*.*(..))")
+	public void returnsCollectionMethods() {
+	}
+
 }
