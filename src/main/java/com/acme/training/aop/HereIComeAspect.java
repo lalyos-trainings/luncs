0d53281dfa5cf579779014f45c99beb062cc4c2b
nethy <bla@bla.com>
2012-12-19 11:41:45 +0100

    HereICome aspect added

---
+++ src/main/java/com/acme/training/aop/HereIComeAspect.java
@@ -0,0 +1,18 @@
+package com.acme.training.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class HereIComeAspect {
+
+    @Before("execution(java.util.Collection com.acme.training..*.*(..))")
+    public void here(JoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature()
+                                     .toString();
+        System.out.format("Here I come ... %s%n", methodName);
+    }
+}
